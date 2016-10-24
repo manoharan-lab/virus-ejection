04043b287526df7a6b82d6e929a6ebdb06aebbfe
rees garmann <rees@reess-macbook-pro.local>
2016-10-24 12:26:37 -0400

    Added RemovePauses function to helpers.py

--- helpers.py
+++ helpers.py
@@ -14,3 +14,19 @@ def movingaverage1D(data, n_pts):
     result[-n_pts/2:] = np.mean(data[-n_pts/2:])
         
     return result
+
+def RemovePauses(datas, pauses):
+    ''' remove pauses from data
+    datas = list of 1D arrays to remove pauses from
+    pauses = list of lists of the start and stop indecies of each pause'''
+	
+    dataps = []
+    for i in range( 0,len(datas) ):
+        data = datas[i]
+        datal = list(data)
+        pause = pauses[i].sort().reverse()
+        for j in range ( 0,len(pause),2 ):
+            del datal[pause[j]:pause[j+1]]
+        dataps.append(np.array(datal))
+	
+    return dataps
